7af1465cd2ec63823cb3728ca22afd95fe31409b
bnikesh02 <bnikesh02@gmail.com>
2021-01-24 17:18:22 -0500

    Merge Commit

--- src/intermediate/myIdm.kt
+++ src/intermediate/myIdm.kt
@@ -9,7 +9,7 @@ class myIdm {
 
     fun to_String(){
         println(squares(5))
-        println("I want to add another print")
+        println("Trying to figure out how merge works")
     }
 
 }
